f73c7462d985897611030068239c964f34d848e5
thrisha <thrishathrishajayanna@gmail.com>
2026-07-10 15:48:13 +0530

    added f3.java in main

---
+++ f3.java
@@ -0,0 +1,11 @@
+package Demo-Project;
+
+public class f3{
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println("Sum = " + (a + b));
+    }
+}
+  
+
